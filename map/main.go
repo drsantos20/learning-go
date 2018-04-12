951d38ce578ed20ebcd771ec910f05c903bfed2b
Daniel Santos <daniel.santos@Daniels-MacBook-Pro.local>
2018-04-11 22:54:53 -0300

    map update

--- map/main.go
+++ map/main.go
@@ -3,10 +3,26 @@ package main
 import "fmt"
 
 func main() {
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+
+	/*
+		colors := make(map[int]string)
+
+		colors[10] = "#ffffff"
+
+		delete(colors, 10)
+	*/
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
